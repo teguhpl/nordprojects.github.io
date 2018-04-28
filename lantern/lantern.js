75cbc36da6d23fe480d79a7e2791e0b9013828c9
Joe Rickerby <joerick@mac.com>
2018-04-28 15:36:01 +0100

    Final videos in vision page

---
+++ lantern/lantern.js
@@ -0,0 +1,31 @@
+var responseStyleElements = $('[data-mobile-style], [data-desktop-style], [data-big-desktop-style]');
+
+responseStyleElements.each(function (i, el) {
+    var $el = $(el);
+    $el.data('original-style', $el.attr('style') || '');
+});
+
+function updateStyle() {
+    responseStyleElements.each(function (i, el) {
+        var $el = $(el);
+        var windowWidth = $(window).width();
+
+        if (windowWidth < 768) {
+            var mobileStyle = $el.data('mobile-style') || '';
+            $el.attr('style', $el.data('original-style')+';'+mobileStyle);
+        } else if (windowWidth < 1200) {
+            var desktopStyle = $el.data('desktop-style') || '';
+            $el.attr('style', $el.data('original-style')+';'+desktopStyle);
+        } else {
+            var desktopStyle = $el.data('desktop-style') || '';
+            var bigDesktopStyle = $el.data('big-desktop-style') || '';
+            $el.attr('style', $el.data('original-style')+';'+desktopStyle+';'+bigDesktopStyle);
+        }
+    })
+}
+
+$(window).resize(function () {
+    updateStyle();
+})
+
+updateStyle();
